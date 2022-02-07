869988aaf77ccd7eb2fc352d14bb68ca12791f9b
pedropaulocg <pedropaulocostaguima@hotmail.com>
2022-02-07 09:43:38 -0300

    animation

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function togglemenu(){
     menu.classList.toggle('active')
 }
 
+var btn = document.querySelector('.btn1')
 
+function animation(){
 
+    btn.classList.toggle('animate')
 
+}
+
+setInterval(animation,2000)
